70b4ddbdfbf8982a82e33fc2378da15b538ef7d6
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-17 15:25:27 +0300

    Update included:   - Made use of the http module to create aserver to run my application on   - Use switch..case conditionals to handle the different routes   - specified the port from which the project should be run on

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ function handleRequests(path, res) {
   })
 }
 
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var path = url.parse(req.url).pathname;
+  switch(path) {
+    case '/':
+      handleRequests('./index.html', res);
+      break;
+    case '/about':
+      handleRequests('./about.html', res);
+      break;
+    case '/contact':
+      handleRequests('./contact.html', res);
+      break;
+    default:
+      res.writeHead(404);
+      res.write('Route was not found');
+      res.end()
+  }
+}).listen(8080, function() {
+  console.log("server start at port 8080");
+});
